69476c4ca683f573913ed23e6dc79dd4a72d800c
Riva7 <37369483+Riva7@users.noreply.github.com>
2018-05-24 10:50:18 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>WanderLand Home</title>
+<link rel="stylesheet" type="text/css" href="Home_Trial.css" />
+</head>
+
+
+<body class="Home" >
+	
+	<div class="Width">
+		<div class="Main">
+		
+		<header>
+			<div class="Title">
+    			<a href="#"><h1 class="Active" id="Title">WanderLand</h1></a>
+				<div class="Access">
+					
+						<a  href="Login.php" class="Login">User Login</a>
+						<a  href="Guest_Home.php" class="Login">Visit The Site</a>
+						<a  href="Admin_login.php" class="Login">Admin</a>
+					
+				</div>
+		</header>				
+				
+			</div>
+			<div class="Intro" id="index">
+		
+				<center><h1 class="IntroText">Discover Places</h1><h3>The Best Guide to your favourite cities</h3></center>
+	</div>
+			
+			<!--<div class="Img">
+				<article>
+					
+					<img src="nature.jpg" />
+				</article>
+			</div>-->
+				
+	
+		
+		<footer class="footer-distributed">
+
+			<div class="footer-left">
+
+				<p class="footer-links">
+					
+					
+					<a href="About_us_guest.php">About</a>.<a href="FAQ_guest.php">Faq</a>.<a href="Gallery_guest.php">Gallery</a>
+					
+				</p>
+
+				<p class="footer-company-name">WanderLand &copy; 2018</p>
+
+			</div>
+
+			<div class="footer-right">
+
+				<form method="get" action="#">
+					<input placeholder="Search our website" name="search" />
+					<i class="fa fa-search"></i>
+				</form>
+
+			</div>
+
+		</footer>
+		</div>
+	</div>
+
+</body>
+</html>
